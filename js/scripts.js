d96b87c307d7828e3de71b85a5f39550ab173bc5
David Monarrez and Isaac Hall <isaac@>
2019-01-17 09:22:31 -0800

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+function canVote(age) {
+  if (age >= 18) {
+    return true
+  } else {
+    return false
+  }
+}
+
+$(document).ready(function () {
+  var age = parseInt(prompt("What is your age"));
+
+
+
+
+
+
+
+
+});
