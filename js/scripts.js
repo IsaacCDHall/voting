38da56a2b20c0825ec5a921f378c7ddf53cd0c1d
David Monarrez and Isaac Hall <david@>
2019-01-17 10:17:51 -0800

    add show/hide features and branching logic

--- js/scripts.js
+++ js/scripts.js
@@ -6,14 +6,19 @@ function canVote(age) {
   }
 }
 
-$(document).ready(function () {
+$(document).ready(function (event) {
+
   var age = parseInt(prompt("What is your age"));
-
-
-
-
-
-
-
+  console.log('test');
+  if (canVote(age) === true) {
+    //show voting info
+    $('.minor').hide();
+    $('.adult').show();
+  } else {
+    //shwo minor links to more voting info
+    $('.adult').hide();
+    $('.minor').show();
+  }
+  event.preventDefault();
 
 });
